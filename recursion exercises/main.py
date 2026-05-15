25102622485dc6148bf45849a193660a76a0ec03
Phillip Madadangoma <156221423+28promad@users.noreply.github.com>
2026-05-15 13:37:49 +0200

    feat: add sum_o function to calculate sum of elements at even indices in a list

--- recursion exercises/main.py
+++ recursion exercises/main.py
@@ -32,6 +32,13 @@ def hourglass(text):
     if len(text) == 1:return text
     return f"{text}\n{hourglass(text[:-1])}\n{text}"
 
+def sum_o(l:list, i=0):
+    if i == len(l):return 0
+    if not i % 2:
+        return l[i] + sum_o(l,i+1)
+    else:
+        return 0 + sum_o(l, i+1)
+
 def main():
     print(factorial(5))
     print(sum_n(10))
@@ -39,6 +46,8 @@ def main():
     print(power(2,5))
     print(reverse_str("steak"))
     print(hourglass("steak"))
+    print(sum_o([1,2,3,4,5]))
+    print(sum_o([1,2,3,4,5,6, 7]))
 
 if __name__ == "__main__":
     main()
